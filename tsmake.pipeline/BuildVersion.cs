2e3003de7f53185bc19c7d6566ac87fb514012c7
Michael K. Campbell <mike@overachiever.net>
2021-04-03 20:34:51 -0700

    Addition of pipeline/processing STUBs
    don't even have a high-level workflow outlined at this point.

---
+++ tsmake.pipeline/BuildVersion.cs
@@ -0,0 +1,20 @@
+﻿using tsmake.Interfaces.Core;
+using tsmake.Interfaces.Enums;
+
+namespace tsmake.Pipeline
+{
+	public class BuildVersion : IBuildVersion
+	{
+		public VersionScheme VersionScheme => throw new System.NotImplementedException();
+
+		public string Major => throw new System.NotImplementedException();
+
+		public string Minor => throw new System.NotImplementedException();
+
+		public string Handle => throw new System.NotImplementedException();
+
+		public string Build => throw new System.NotImplementedException();
+
+		public string VersionSummary => throw new System.NotImplementedException();
+	}
+}
