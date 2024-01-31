90aecc4c97122b8ec783053a83dc972f05fc63d7
Michael K. Campbell <mike@overachiever.net>
2024-01-31 12:06:48 -0800

    Core Project Files

---
+++ tsmake.TempPoint.psm1
@@ -0,0 +1,20 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2023 v5.8.235
+	 Created on:   	1/30/2024 6:34 PM
+	 Created by:   	Michael K. Campbell
+	 Organization: 	OverAchiever Productions, LLC.
+	 Filename:     	tsmake.psm1
+	-------------------------------------------------------------------------
+	 Module Name: tsmake
+	===========================================================================
+#>
+
+
+
+function Write-HelloWorld
+{
+	Write-Host "Hello World"
+}
+
+
