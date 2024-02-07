1a46c119901951980c2582fec0db5b2b983d42e5
Michael K. Campbell <mike@overachiever.net>
2024-02-07 10:43:45 -0800

    Addition of (VS) Unit Tests and other CLR/Project Changes.

---
+++ clr/tsmake.tests/unit tests/LineTests.cs
@@ -0,0 +1,49 @@
+﻿using tsmake.enums;
+
+namespace tsmake.tests.unit_tests;
+
+[TestFixture]
+public class LineTests
+{
+    [Test]
+    public void WhiteSpaceLine_Is_Both_Raw_And_WhiteSpace()
+    {
+        string emptyLine = "";
+        var sut = new Line(11, emptyLine, "some file.sql");
+
+        Assert.That(sut.LineType.HasFlag(LineType.RawContent));
+        Assert.That(sut.LineType.HasFlag(LineType.WhitespaceOnly));
+    }
+
+    [Test]
+    public void Directive_Is_Directive()
+    {
+        string directiveLine = @"--##OUTPUT: \\Deployment";
+
+        var sut = new Line(11, directiveLine, "some file.sql");
+
+        Assert.That(sut.LineType.HasFlag(LineType.Directive));
+    }
+
+    [Test]
+    public void Directive_Is_Not_WhiteSpace()
+    {
+        string directiveLine = @"--##OUTPUT: \\Deployment";
+
+        var sut = new Line(11, directiveLine, "some file.sql");
+
+        // wth?
+        Assert.False(sut.LineType.HasFlag(LineType.WhitespaceOnly));
+    }
+
+    [Test]
+    public void Directive_Is_Not_RawContent()
+    {
+        string directiveLine = @"--##OUTPUT: \\Deployment";
+
+        var sut = new Line(11, directiveLine, "some file.sql");
+
+        // wth?
+        Assert.False(sut.LineType.HasFlag(LineType.RawContent));
+    }
+}
