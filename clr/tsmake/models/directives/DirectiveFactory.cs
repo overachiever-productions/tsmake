1998af694bd81bd2bf19be341dd48a5b6476d0f2
Michael K. Campbell <mike@overachiever.net>
2024-02-08 18:55:50 -0800

    Refactored DirectiveInstance .ctors.

--- clr/tsmake/models/directives/DirectiveFactory.cs
+++ clr/tsmake/models/directives/DirectiveFactory.cs
@@ -8,18 +8,17 @@ namespace tsmake.models.directives
         {
             switch (directiveName)
             {
-                // REFACTOR: I need to work on the .ctor/signtatures for IDirectiveInstances a bit more - i.e., I think I should pass in the entire Line object ... vs just location + content. 
                 case "OUTPUT":
-                    return new OutputDirective(directiveName, line.Content, location);
+                    return new OutputDirective(directiveName, line, location);
                 case "FILE":
-                    return new IncludeFileDirective(directiveName, line.Content, location);
+                    return new IncludeFileDirective(directiveName, line, location);
                 case "DIRECTORY":
-                    return new IncludeDirectoryDirective(directiveName, line.Content, location);
+                    return new IncludeDirectoryDirective(directiveName, line, location);
                 case "NOTE": // or COMMENT... 
                     // TODO: need to work on this - don't think I want to require ##NOTE as the actual ... directive.... i think ##: or ##:: might just be it? 
                     //      i.e., I'm going to have to 1) figure out what I want for syntax rules/options and 2) implement - which'll probably mean some sort of 
                     //      semi-hack/work-around where I look for, say, a) normal directives, and then b) ##:: as a specialized directive and .. if I find it ... 'mark' it in some way so that it can get passed into this... 
-                    return new CommentDirective(directiveName, line.Content, location);
+                    return new CommentDirective(directiveName, line, location);
                 default:
                     throw new InvalidCastException($"Unknown Directive: {directiveName}.");
             }
--- clr/tsmake/models/directives/DirectiveInstance.cs
+++ clr/tsmake/models/directives/DirectiveInstance.cs
@@ -3,63 +3,60 @@
     public interface IDirectiveInstance
     {
         string Name { get; }
-        string RawData { get; }
+        Line Line { get; }
         Location Location { get; }
+
+        // bool IsValid { get; }
     }
 
     public abstract class BaseDirectiveInstance : IDirectiveInstance
     {
         public string Name { get; }
-        public string RawData { get; }
+        public Line Line { get; }
         public Location Location { get; }
 
-        protected BaseDirectiveInstance(string name, string rawData, Location location)
+        protected BaseDirectiveInstance(string name, Line line, Location location)
         {
             this.Name = name;
-            this.RawData = rawData;
+            this.Line = line;
             this.Location = location;
         }
     }
 
-    //public class SomethingDirective : BaseDirectiveInstance
-    //{
-    //    public SomethingDirective(string name, string rawData, Location location) : base(name, rawData, location) {}
-    //}
-
     public class RootPathDirective : BaseDirectiveInstance
     {
-        public RootPathDirective(string name, string rawData, Location location) : base(name, rawData, location) { }
+        public RootPathDirective(string name, Line line, Location location) : base(name, line, location) { }
     }
 
     public class OutputDirective : BaseDirectiveInstance
     {
-        public OutputDirective(string name, string rawData, Location location) : base(name, rawData, location) { }
+        public OutputDirective(string name, Line line, Location location) : base(name, line, location) { }
     }
 
     public class CommentDirective : BaseDirectiveInstance
     {
-        public CommentDirective(string name, string rawData, Location location) : base(name, rawData, location) { }
+        public CommentDirective(string name, Line line, Location location) : base(name, line, location) { }
     }
 
     public class VersionCheckerDirective : BaseDirectiveInstance
     {
-        public VersionCheckerDirective(string name, string rawData, Location location) : base(name, rawData, location) { }
+        public VersionCheckerDirective(string name, Line line, Location location) : base(name, line, location) { }
     }
 
     public class IncludeFileDirective : BaseDirectiveInstance
     {
-        public IncludeFileDirective(string name, string rawData, Location location) : base(name, rawData, location) { }
+        public IncludeFileDirective(string name, Line line, Location location) : base(name, line, location) { }
     }
 
     public class IncludeDirectoryDirective : BaseDirectiveInstance
     {
-        public IncludeDirectoryDirective(string name, string rawData, Location location) : base(name, rawData, location) { }
+        public IncludeDirectoryDirective(string name, Line line, Location location) : base(name, line, location) { }
     }
 
     public class ConditionalBlockDirective : BaseDirectiveInstance
     {
         // not sure if this'll be a single directive, or a 'family' of 4x directives... 
 
-        public ConditionalBlockDirective(string name, string rawData, Location location) : base(name, rawData, location) { }
+        public ConditionalBlockDirective(string name, Line line, Location location) : base(name, line, location) { }
     }
 }
