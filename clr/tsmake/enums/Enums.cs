6f613b2ba97a790b9bd2eec84243ebf5a8477545
Michael K. Campbell <mike@overachiever.net>
2024-02-13 12:11:11 -0800

    Consolidation of Enums into single file.

---
+++ clr/tsmake/enums/Enums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace tsmake
+{
+    public enum VersionScheme
+    {
+        FourPart,
+        Semantic,
+        Organic
+    }
+
+    public enum PathType
+    {
+        Absolute,
+        Relative, 
+        Rooted
+    }
+
+    public enum OrderBy
+    {
+        Alphabetical, 
+        ModifyDate,
+        CreateDate 
+    }
+
+    public enum Direction
+    {
+        Ascending, 
+        Descending
+    }
+
+    [Flags]
+    public enum LineType
+    {
+        RawContent = 1,
+        WhitespaceOnly = 2,
+        TokenizedContent = 4,
+        Directive = 8
+    }
+
+}
