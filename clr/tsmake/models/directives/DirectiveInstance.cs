fad6274ff64988368fe958ca9fdc7cf0bdc1b01c
Michael K. Campbell <mike@overachiever.net>
2024-02-20 08:10:07 -0800

    Significatn workflow changes/mods.

--- clr/tsmake/models/directives/DirectiveInstance.cs
+++ clr/tsmake/models/directives/DirectiveInstance.cs
@@ -1,10 +1,14 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
 namespace tsmake.models.directives
 {
+    // REFACTOR: I'm getting old and stupid. Tokens can/will have TokenDefinitions and TokenInstances - cuz end-users are allowed
+    //      to define tokens. End-users can't define Directives... so, i don't really need to differentiate between Directives and Directive 'Instances'. 
+    //      in short, these can simply become Directives.
     public interface IDirectiveInstance
     {
         string DirectiveName { get; }
@@ -42,31 +46,43 @@ namespace tsmake.models.directives
         {
             base.DirectiveName = "ROOT";
 
-            // Two primary options for RootPath: a) hard-coded/absolute path, or b) a relative path (from xxx.build.sql file).
-            // TODO: arguably.... sigh, \\unc-paths\could-be\legit\as-well\
-            if(Regex.Match(line.Content, @"[A-Za-z]:\\") != null)
-            {
-                this.PathType = PathType.Absolute;
-                base.IsValid = true;
-                return;
-            }
+            string data = this.GetDirectiveLineData();
 
-            if(Regex.Match(line.Content, @"(\.\.\\)+|[A-Za-z\.]+") != null)
+            // Two primary options for RootPath: a) hard-coded/absolute path (e.g., local windows path like F:\blah, OR a UNC file-share path), or b) a relative path (from xxx.build.sql file).
+            if (data.IsValidPath())
             {
-                this.PathType = PathType.Absolute;
-                base.IsValid = true;
-                return;
-            }
+                // TODO: don't allow PathType.Rooted for ... the Root. Those can ONLY be used once this is set.
+                this.PathType = data.GetPathType();
+                this.Path = data;
 
-            base.ValidationMessage = $"Invalid or missing path-directive for Directive [ROOT] in file: {location.FileName}, line: {location.LineNumber}.";
+                this.IsValid = true;
+            }
+            else 
+                base.ValidationMessage = $"Invalid (or missing) File-Path Data for Directive [ROOT] in file: {location.FileName}, line: {location.LineNumber}.";
         }
     }
 
     public class OutputDirective : BaseDirectiveInstance
     {
+        public string Path { get; }
+        public PathType PathType { get; }
+
         public OutputDirective(Line line, Location location) : base(line, location)
         {
             base.DirectiveName = "OUTPUT";
+
+            string data = this.GetDirectiveLineData();
+
+            // Two primary options for RootPath: a) hard-coded/absolute path (e.g., local windows path like F:\blah, OR a UNC file-share path), or b) a relative path (from xxx.build.sql file).
+            if (data.IsValidPath())
+            {
+                this.PathType = data.GetPathType();
+                this.Path = data;
+
+                this.IsValid = true;
+            }
+            else 
+                base.ValidationMessage = $"Invalid (or missing) File-Path Data for Directive [OUTPUT] in file: {location.FileName}, line: {location.LineNumber}.";
         }
     }
 
@@ -74,7 +90,7 @@ namespace tsmake.models.directives
     {
         public CommentDirective(Line line, Location location) : base(line, location)
         {
-            base.DirectiveName = "::";
+            base.DirectiveName = "COMMENT";
             // And... done. 
             // As in, there's NOTHING to do here.
         }
@@ -85,11 +101,28 @@ namespace tsmake.models.directives
         public VersionCheckerDirective(string name, Line line, Location location) : base(line, location) { }
     }
 
+    // REFACTOR: IncludeFileDirective, RootPathDirective, and OutputDirective all have - for all intents and purposes - the SAME underlying functionality IN THE .CTOR
+    //      in essence, they all: a) get the directive data/input, b) check to see if it's a valid path and assign it + type if it is, c) throw an exception if not valid path. 
+    //      ultimately, in terms of .ctor logic - the ONLY thing that's different is the error message. 
     public class IncludeFileDirective : BaseDirectiveInstance
     {
+        public string Path { get; }
+        public PathType PathType { get; }
+
         public IncludeFileDirective(Line line, Location location) : base(line, location)
         {
             base.DirectiveName = "FILE";
+
+            string data = this.GetDirectiveLineData();
+            if (data.IsValidPath())
+            {
+                this.PathType = data.GetPathType();
+                this.Path = data;
+
+                this.IsValid = true;
+            }
+            else 
+                base.ValidationMessage = $"Invalid (or missing) File-Path Data for Directive [INCLUDEFILE] in file {location.FileName}, line: {location.LineNumber}.";
         }
     }
 
@@ -110,7 +143,7 @@ namespace tsmake.models.directives
             this.Priorities = new List<string>();
             this.UnPriorities = new List<string>();
 
-            string data = line.Content.Substring(location.ColumnNumber + base.DirectiveName.Length + 1); // +1 is for the :
+            string data = this.GetDirectiveLineData();
 
             Dictionary<string, string> components = new Dictionary<string, string>();
 
@@ -123,30 +156,26 @@ namespace tsmake.models.directives
 
                 for (int i = 0; i < count; i++)
                 {
-                    var part = parts[i];
-                    int start = part.Index;
+                    int start = parts[i].Index;
                     int end = data.Length;
                     if (i < count - 1) 
                         end = parts[i + 1].Index;
 
-                    key = part.Value.Replace(":", "");
-                    segment = data.Substring(start, end - start).Replace(key, "").Trim();
+                    key = parts[i].Value.Replace(":", "");
+                    segment = data.Substring(start, end - start).Replace(key, "").Replace(":", "").Trim();
 
-                   components.Add(key, segment);
+                    components.Add(key, segment);
                 }
 
                 if (!components.ContainsKey("PATH"))
                 {
                     segment = data.Substring(0, parts[0].Index);
-                    key = "PATH";
-
-                    components.Add(key, segment);
+                    components.Add("PATH", segment);
                 }
             }
             else
             {
-                // either ... the directive is equivalent to something like -- ## DIRECTORY: <path_here_and_nothing_else> 
-                //      or, it's invalid... 
+                // if we didn't match on sub-directives, then either: a) this is a simple DIRECTORY directive with <path_and_nothing_else> or, b) it's invalid.
                 string directoryPath = data.Trim();
                 
                 // TODO: now ... check to see if [data] could be a path/directory (without, necessarily) checking it... 
@@ -165,7 +194,7 @@ namespace tsmake.models.directives
                 }
             }
 
-            // TODO: ensure that I don't need to check this ... think i probably do... 
+            // TODO: ensure that I don't need to (error-check that this isn't null) check this ... think i probably do... 
             this.Path = components["PATH"];
             this.PathType = this.Path.GetPathType();
 
@@ -174,13 +203,13 @@ namespace tsmake.models.directives
                 data = components["ORDERBY"];
 
                 // NOTE: not even 'bothering' to check for entire 'words' here - just enough to know that the intention is/was specified (or not).
-                if (data.Contains("modi"))
+                if (data.ToLowerInvariant().Contains("modi"))
                 {
                     this.OrderBy = OrderBy.ModifyDate;
                 }
                 else
                 {
-                    if (data.Contains("crea")) 
+                    if (data.ToLowerInvariant().Contains("crea")) 
                     {
                         this.OrderBy = OrderBy.CreateDate;
                     }
@@ -188,7 +217,7 @@ namespace tsmake.models.directives
                         this.OrderBy = OrderBy.Alphabetical; // note that this ends up being the default (i.e., if nothing is explicitly specified).
                 }
 
-                if (data.Contains("desc"))
+                if (data.ToLowerInvariant().Contains("desc"))
                     this.Direction = Direction.Descending;
                 else 
                     this.Direction = Direction.Ascending;  // also the default - if nothing is explicitly specified.
@@ -196,19 +225,28 @@ namespace tsmake.models.directives
 
             if (components.ContainsKey("EXCLUDE"))
             {
-                this.Exclusions.AddRange(components["EXCLUDE"].Split(",")); 
+                this.Exclusions.AddRange(components["EXCLUDE"].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim())); 
             }
 
             if (components.ContainsKey("PRIORITIES"))
             {
-                string[] priorities = components["PRIORITIES"].Split(";");
+                string[] priorities = components["PRIORITIES"].Split(";", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
 
-                this.Priorities.AddRange(priorities[0].Split(","));
-
-                // TODO: there's arguably a bug/problem here if .Length > 1 (i.e., that'd mean 2x (or more) semi-colons - which should be a syntax error. 
-                //      i.e., only valid options should be length of 0 (nothing was entered), 1 (only priorities were set), or 2 (priorities and unpriorities were specified).
-                if (priorities.Length > 1)
-                    this.UnPriorities.AddRange(priorities[1].Split(","));
+                switch (priorities.Length)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        this.Priorities.AddRange(priorities[0].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()));
+                        break;
+                    case 2:
+                        this.Priorities.AddRange(priorities[0].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()));
+                        this.UnPriorities.AddRange(priorities[1].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()));
+                        break;
+                    default:
+                        this.ValidationMessage = "Invalid Syntax for PRIORITIES: - multiple semi-colons (;) found - should only be 1.";
+                        return;
+                }
             }
 
             this.IsValid = true;
--- internal/Pipeline.ps1
+++ internal/Pipeline.ps1
@@ -27,7 +27,7 @@ function Execute-Pipeline {
 		Write-Debug "$(Get-PipelineDebugIndent -Key "Root")Starting Pipeline Operations.";
 		Write-Verbose "Starting Pipeline Operations.";
 		
-		# create an errorS (array) object?
+		[tsmake.BuildResult]$buildResult = New-Object tsmake.BuildResult($BuildContext.BuildFile);  
 	};
 	
 	process {
@@ -36,32 +36,199 @@ function Execute-Pipeline {
 		# ====================================================================================================	
 		[tsmake.models.BuildManifest]$buildManifest = [tsmake.models.BuildManifest]($BuildContext.BuildFile);
 		
-		foreach ($line in $buildManifest.Lines) {
-			#write-host "$($line.LineNumber)  -> $($line.LineType)";
-						
-#			if ($line.LineType.HasFlag([tsmake.enums.LineType]::WhitespaceOnly)) {
-#				Write-Host "$($line.LineNumber)  -> $($line.Content)";
-#			}
-#			if ($line.LineType.HasFlag([tsmake.enums.LineType]::RawContent)) {
-#				if (-not ($line.LineType.HasFlag([tsmake.enums.LineType]::WhitespaceOnly))) {
-#					Write-Host "$($line.LineNumber)  -> $($line.Content)";
-#				}
-#			}
+		# ====================================================================================================
+		# 2. Check-for and Report on (or Handle) and ParserErrors:
+		# ====================================================================================================		
+		if ($buildManifest.ParserErrors.Count -gt 0) {
 			
+			# REFACTOR: probably do a better job of handing off errors/details from one model to the next here (i.e., just have buildManifest hand off to results or whatever)
+			#  	and then, if we're in a 'fatal' state or found a fatal set of errors, bail... 
 			
-#			if ($line.LineType.HasFlag([tsmake.enums.LineType]::Directive)) {
-#				Write-Host "$($line.LineNumber)  -> $($line.Content)";
-#			}
+			# list/process any FATAL errors first
+			$fatalErrors = $false;
+			foreach($error in $buildManifest.ParserErrors | Where-Object { "FATAL" -eq $_.Severity }) {
+				$fatalErrors = $true;
+				
+				Write-Verbose "Fatal Parsing Error: $($error.ErrorMessage)";
+				$buildResult.AddProcessingError($error);
+			}
 			
-#			if ($line.LineType.HasFlag([tsmake.enums.LineType]::TokenizedContent)) {
-#				
-#				Write-Host "$($line.LineNumber)  -> $($line.Content)";
-#				foreach ($t in $line.Tokens) {
-#					Write-Host "	TokenName: $($t.Name)  -> Value: $($t.DefaultValue)  -> Location: $($t.LineNumber), $($t.Position) ";
-#				}
-#			}
+			# TODO: MIGHT want to NOT spit these out IF -StopOnFirstError (or whatever I'm going to call it) is set... 
+			foreach($error in $buildManifest.ParserErrors | Where-Object { "FATAL" -ne $_.Severity }) {
+				$fatalErrors = $true;
+				
+				Write-Verbose "Parser Warning: $($error.ErrorMessage)";
+			}
+			
+			if ($fatalErrors) {
+				return;  # which actually sends us out to the end {} block 
+			}
 		}
 		
+		# ====================================================================================================
+		# 3. Get / Evaluate Global Directives (ROOT, OUTPUT, FILEMARKER, VERSIONCHECKER, etc.)
+		# ====================================================================================================		
+		$fatalError = $null;
+		if ($null -ne $buildManifest.RootDirective) {
+			$root = $buildManifest.RootDirective;
+			Write-Verbose "	RootPath Directive found. Path: [$($root.Path)] => PathType: $($root.PathType). Location: $($root.Location.LineNumber), $($root.Location.ColumnNumber).";
+			
+			switch ($root.PathType) {
+				"Absolute" {
+					if (-not (Test-Path -Path ($root.Path))) {
+						$fatalError = New-ParserError -Severity "Fatal" -Location ($root.Location) -ErrorMessage "Specified Root Path: [$($root.Path)] does NOT exist.";
+					}
+					
+					Write-Verbose "		RootPath Explicitly Set to: [$($root.Path)].";
+					# TODO: Set .BuildCOntext.RootPath to ... newly defined root path. 
+					# 	only... in order to do the above... i'll need an actual object - with a couple of helper methods or whatever... (i.e., don't want to directly manipulate the object from here).	
+				}
+				"Relative" {
+					$translatedRootPath = Translate-Path -CurrentPath ($BuildContext.WorkingDirectory) -PathDirective ($root.Path);
+					
+					if (-not (Test-Path -Path $translatedRootPath)) {
+						$fatalError = New-ParserError -Severity "Fatal" -Location ($root.Location) -ErrorMessage "Specified Root Path: [$($translatedRootPath)] does NOT exist.";
+					}
+					
+					Write-Verbose "		RootPath Explicitly Set to: [$($translatedRootPath)].";
+					
+					# TODO: set path (as per above).
+				}
+				"Rooted" {
+					$fatalError = New-ParserError -Severity "Fatal" -Location ($root.Location) -ErrorMessage "Rooted Paths are NOT allowed for RootPath Directives.";
+				}
+				default {
+					$msg = "Unknown PathType: [$($root.PathType)] specified for RootPath Directive.";
+					$fatalError = New-ParserError -Severity "Fatal" -Location ($root.Location) -ErrorMessage $msg;
+				}
+			}
+			
+			if ($null -ne $fatalError) {
+				return;
+			}
+		}
+		
+		if ($null -ne $buildManifest.OutputDirective) {
+			$outputDirective = $buildManifest.OutputDirective;
+			Write-Verbose "	Output Directive found. xxxx";
+			
+			if ($null -eq $BuildContext.Output) {
+				# TODO: 3x tasks:
+				# 1. build/define (relative or absolute or rooted) path... 
+				# 2. test it. 
+				# 3. assign it. 
+				#  then... Write-Verbose about how it was set... 
+			}
+			else {
+				Write-Verbose "		-Output Specified via Command Line supersedes ##OUTPUT directive set within .build file.";
+			}
+			
+			if ($null -eq $BuildContext.Output) {
+				#$fatalError = New-RuntimeError 
+			}
+			
+			if ($null -ne $fatalError) {
+				return;
+			}
+		}
+		
+		# TODO: VERSION_CHECKER
+		# TODO: FILEMARKER
+		# TODO: anything else? 
+		
+		# ====================================================================================================
+		# 4. Process all INCLUDES (FILE/DIRECTORY/RECURSIVE... )
+		# ====================================================================================================			
+		
+		# Start by Validating and Assigning Actual Paths:
+		foreach($include in $buildManifest.Directives | Where-Object { $_.DirectiveName -in ("FILE", "DIRECTORY")}){
+			if (-not ($include.IsValid)) {
+				Write-Host "TODO: Add a parser validation error with line/location and message = $($include.ValidationMessage).";
+				# todo add some context to the above... actually, the context will be the line/location and the filename the error came from.
+			}
+			else {
+				# check the path type... 	
+				Write-Host "TODO: check/validate actual path + store for: $($include.DirectiveName) -> ($($include.PathType)) $($include.Path)";
+				
+				# use .PathType + .Path to create the .ConcretePath (or whatever). 
+				#  check to see if .ConcretePath is valid. Since we're dealing with files and directories (i.e., includes), each file/directory 
+				# 			should exist at this point. 
+				# 		if it doesn't, need to add a 'ParserError' (though, hmm... it's not actually a PARSER error)
+				# 			maybe there are IProcessingErrors of types ParsingError, Execution/RuntimeError (which is what this'd be), and ... i dunno, logic (dynamic directives, etc.) or whatever errors? 
+				# 		yeah... do the above - i.e., have different error types. ParserErrors, RuntimeErrors, Write/OutputErrors, MigrationErrors, Generator(from .git or whatever)Errors, and the likes. 
+				# 			they can all use VERY similar, underlying, functionality and interfaces + a base type ... but, they should be different TYPES of errors. 
+				
+				#  otherwise, IF the .ConcretePath truly exists: 
+				# 		$include.SetConcretePath($concretePath)
+				# 			which'll also set some sort of .IsReallyValid = true as well... 
+			}
+			
+			# once the above is done... (and assuming that we're not in -EagerFailure = $true (or -StopOnFirstError - i.e., whatever I call it))
+			# 		then go ahead and, for each .IsTrulyValid INCLUDE directive... 
+			# 			start (recursively) COPYING content from $buildFile (replacement name for $buildManifest) into NewBuildManifest or ExpandoManifest - whatever I'm going to call it.
+			# 				where RECURSIVELY means ... for each FileManifest or DirectoryManifest that I create ... pull in the contents, look for FILE/DIRECTORY includes in those... and, if found... recurse on down. 
+# 	NOTE: 	FileManifests will be what end up being used for generating IN-FILE documentation. 
+# 		 not sure that HAS TO BE processed here (chronologically/logicallty) 
+# 		BUT: FileManifests will have processed CommentPreferences (i.e., remove top /* 1x header comment */ or /* all header commnets */
+# 			 (where 'removed' means: will NOT have output into the Build/ExpandoManifest ... vs actually deleting/removing any actual text. 
+# 				point being: after loading all FILE includes ... i'll have everything i need (within the collection of FileManifests) to grab/parse/build/output IN-FILE docs. 
+			# 
+			#  	otherwise, once we're done (assuming we didn't run into any errors along the way... )
+			# 		we've now got an 'expando' or Build Manifest - something that's ENTIRELY done except for: 
+			# 			a) conditional processing/logic.
+			# 			b) tokens. 
+			# 	translation: 
+			# 			from this point on I then need to process: 
+			# 		a) conditional logic
+			# 		b) tokens
+			# 		c) i was going to say: IN-FILE documentation - but, NOPE, that'll have been done up in FileManifests. (Crazy)
+			# 		d) writing output to OUTPUT.xxx 
+			#   	e) file-marker content/output. 
+			# 		f) spitting back results on stats and the overall outcome and the likes. 
+			
+			
+			
+			
+			
+		}
+		
+		#		foreach ($line in $buildManifest.Lines) {
+		#			#write-host "$($line.LineNumber)  -> $($line.LineType)";
+		#						
+		##			if ($line.LineType.HasFlag([tsmake.enums.LineType]::WhitespaceOnly)) {
+		##				Write-Host "$($line.LineNumber)  -> $($line.Content)";
+		##			}
+		##			if ($line.LineType.HasFlag([tsmake.enums.LineType]::RawContent)) {
+		##				if (-not ($line.LineType.HasFlag([tsmake.enums.LineType]::WhitespaceOnly))) {
+		##					Write-Host "$($line.LineNumber)  -> $($line.Content)";
+		##				}
+		##			}
+		#			
+		#			
+		##			if ($line.LineType.HasFlag([tsmake.enums.LineType]::Directive)) {
+		##				Write-Host "$($line.LineNumber)  -> $($line.Content)";
+		##			}
+		#			
+		##			if ($line.LineType.HasFlag([tsmake.enums.LineType]::TokenizedContent)) {
+		##				
+		##				Write-Host "$($line.LineNumber)  -> $($line.Content)";
+		##				foreach ($t in $line.Tokens) {
+		##					Write-Host "	TokenName: $($t.Name)  -> Value: $($t.DefaultValue)  -> Location: $($t.LineNumber), $($t.Position) ";
+		##				}
+		##			}
+		#		}		
+		
+		
+		
+		#		
+#		if ($buildManifest.Directives.ContainsKey("ROOT")) {
+#			Write-Host "found a ROOT..."
+#		}
+#		else {
+#			Write-Host "didn't find a ROOT"
+#		}
+		
+		$buildResult.SetSucceeded(); # just pretend for now... 
 		
 		# ====================================================================================================
 		# X. ... NEXT
@@ -71,6 +238,8 @@ function Execute-Pipeline {
 		# now that we've gotten a list of ALL directives: 
 		# 	1. look for a ROOT. No biggie if we don't have one - it'll be the path/folder where the current .build.sql file is. 
 		# 		but, if there is one, try to establish it and so on... i.e., validate and everything. 
+		
+		
 		# 	  1.a ...do the same for ... OUTPUT, FILEMARKER and an others... 
 		# 	  1.b Ah yeah... ##VERSION_CHECKER should be processed here - i.e., either there is one and I need to find the code for what was specified. 
 		# 			or.... i spam in the tsmake 'default' version-checker ... that'll also, presumably? get dropped at the end? 
@@ -114,11 +283,11 @@ function Execute-Pipeline {
 		}
 		
 		
-		Write-Host "-------------------------------------------------------";
-		Write-Host "Count: $($buildManifest.Directives.Count)"
-		foreach ($d in $buildManifest.Directives) {
-			Write-Host "Directive Location:  $($d.Location.LineNumber), $($d.Location.ColumnNumber) -> DirectiveName: $($d.Name)"
-		}
+#		Write-Host "-------------------------------------------------------";
+#		Write-Host "Count: $($buildManifest.Directives.Count)"
+#		foreach ($d in $buildManifest.Directives) {
+#			Write-Host "Directive Location:  $($d.Location.LineNumber), $($d.Location.ColumnNumber) -> Name: $($d.DirectiveName)"
+#		}
 		
 		
 		# ====================================================================================================
@@ -138,7 +307,7 @@ function Execute-Pipeline {
 	};
 	
 	end {
-		
+		return $buildResult;
 	};
 }
 
