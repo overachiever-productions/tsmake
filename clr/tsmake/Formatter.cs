b222b8a9c50c147411530266a40648a51017a2e7
Michael K. Campbell <mike@overachiever.net>
2024-02-20 08:07:51 -0800

    Initial introduction of output/formatting.

---
+++ clr/tsmake/Formatter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Management.Automation;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace tsmake
+{
+    public class Formatter
+    {
+        private Formatter() { }
+
+        public bool HostSupportsColor { get; set; } // exposing this as a PUBLIC prop - not sure I'm going to use it much from 'outside' this class though.
+        public static Formatter Instance => new Formatter();
+
+        public void SetCurrentHostInfo(string name)
+        {
+            if (name.ToLowerInvariant() == "consolehost")
+                this.HostSupportsColor = true;
+            else
+            {
+                var regex = new Regex("console|code|remotehost");
+                if (regex.IsMatch(name))
+                    this.HostSupportsColor = true;
+            }
+        }
+
+        public string SizedDash(int length)
+        {
+            string output = new String('-', length);
+
+            if (this.HostSupportsColor)
+                //output = $"\u001b[36;1m{output}\u001b[0m";
+                output = $"{PSStyle.Instance.Foreground.BrightCyan}{output}{PSStyle.Instance.Reset}";
+
+            return output;
+        }
+
+        public string ColumnHeading(int leftPadding, string name, int length)
+        {
+            string padding = new String(' ', length);
+            string output = $"{name}{padding}".Substring(0, length);
+
+            if (leftPadding > 0)
+                output = new String(' ', leftPadding) + output;
+
+            if (this.HostSupportsColor)
+                output = $"{PSStyle.Instance.Foreground.BrightCyan}{output}{PSStyle.Instance.Reset}";
+
+            return output;
+        }
+
+        public string ResultOutcome(int leftPadding, IProcessingResult result, string pattern, int length)
+        {
+            string padding = new String(' ', length);
+            string output = $"{string.Format(pattern, result.Outcome.ToString().ToUpperInvariant())}{padding}".Substring(0, length);
+
+            if (leftPadding > 0)
+                output = new String(' ', leftPadding) + output;
+
+            if (this.HostSupportsColor)
+            {
+                output = $"{PSStyle.Instance.Foreground.Red}{output}{PSStyle.Instance.Reset}";
+            }
+
+            return output;
+        }
+    }
+}
+
---
+++ clr/tsmake/ProcessingResult.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tsmake
+{
+    public interface IProcessingResult
+    {
+        // .Start and .End times? 
+        public string OperationType { get; }  // might make more sense to have this as yet-another ENUM? 
+        public ProcessingOutcome Outcome { get; }
+        public List<Errors> ParserErrors { get; }
+
+        // .GetFatalErrors()
+        // .GetAllErrors() ? 
+    }
+
+    public abstract class BaseProcessingResult : IProcessingResult
+    {
+        public string OperationType { get; protected set; }
+        public ProcessingOutcome Outcome { get; private set; }
+        public List<Errors> ParserErrors { get; }
+
+        protected BaseProcessingResult()
+        {
+            this.Outcome = ProcessingOutcome.Failure; // default to failed. 
+            this.ParserErrors = new List<Errors>();
+        }
+
+        public void AddProcessingError(Errors error)
+        {
+            this.ParserErrors.Add(error);
+        }
+
+        public void SetSucceeded()
+        {
+            this.Outcome = ProcessingOutcome.Success;
+        }
+    }
+
+    public class BuildResult : BaseProcessingResult 
+    {
+        // properties that this should have: 
+        // .BuildFile 
+        // ??? 
+        public string BuildFile { get; }
+
+
+        //      GUESSING that I should all all of the ? below via Set Success and/or SetFailure (i.e., even if build fails, I want to know # of lines, tokens, directives, etc)
+        // .TokensCount?   - and... there are 2 potential counts: found, successfully-processed, etc. 
+        // .DirectivesCount?   -- ditto on found/defined vs processed. 
+        // .Lines(total)ProcessedCount ? 
+
+        public BuildResult(string buildFile)
+        {
+            base.OperationType = "BUILD";  // TODO: yeah... this needs to be an enum... 
+            this.BuildFile = buildFile;
+        }
+    }
+
+
+    // then ... DocumentationResult ???
+
+
+    // OTHER, eventual, results will be: 
+    //      MigrationRun (or whatever I call it)
+    //      git/source-controlBuild (or whatever)
+    //  and so on.
+}
