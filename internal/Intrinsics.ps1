254528d35be000a3d09080354bb38050e2ccfc07
Michael K. Campbell <mike@overachiever.net>
2024-02-22 08:50:57 -0800

    New 'intrinsics' file - with 'base objects'.

---
+++ internal/Intrinsics.ps1
@@ -0,0 +1,64 @@
+﻿Set-StrictMode -Version 1.0;
+
+# ====================================================================================================
+# BuildContext
+# 		I MAY turn BuildContext into a CLR object - IF it ends up having enough 'logic' within funcs/etc.
+# ====================================================================================================	
+function New-BuildContext {
+	param (
+		[string]$BuildFile,
+		[string]$Output,
+		[string]$Version,
+		[string]$WorkingDirectory
+	)
+	
+	begin {
+		[ScriptBlock]$setOutputLocation = {
+			param (
+				[Parameter(Mandatory)]
+				[string]$OutputLocation
+			);
+			
+			$this.Output = $OutputLocation;
+		}
+		
+		[ScriptBlock]$setRootLocation = {
+			param (
+				[Parameter(Mandatory)]
+				[string]$RootLocation
+			);
+			
+			$this.Root = $RootLocation;
+		}
+	}
+	
+	process {
+		[PSCustomObject]$context = [PSCustomObject]@{
+			BuildFile	     = $BuildFile
+			Output		     = $Output
+			Version		     = $Version 				# TODO: this should probably be an object (i.e., C# model) at this point... 
+			WorkingDirectory = $WorkingDirectory
+			Root		 	 = "" 						# Build Root. Set later/explicitly.
+			# Verb? Build | Docs | Build+Docs (the option for Docs can only come from Invoke-TsmDocs)
+			# Documentation/Transformer Directives
+		}
+		
+		Add-Member -InputObject $context -MemberType ScriptMethod -Name SetOutput -Value $setOutputLocation;
+		Add-Member -InputObject $context -MemberType ScriptMethod -Name SetRoot -Value $setRootLocation;
+	}
+	
+	end {
+		return $context;
+	}
+}
+
+# ====================================================================================================
+# Token Registry
+# ====================================================================================================	
+$global:tsmTokenRegistry = [tsmake.models.TokenRegistry]::Instance;
+
+# ====================================================================================================
+# Formatter
+# ====================================================================================================	
+$global:TsmFormatter = [tsmake.Formatter]::Instance;
+$TsmFormatter.SetCurrentHostInfo($Host.Name);
