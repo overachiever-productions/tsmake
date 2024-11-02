164ec183311b85a46e8f7be88fc4f438dd8986f4
Michael K. Campbell <mike@overachiever.net>
2024-11-02 13:05:39 -0700

    Initial / Bare-Bones Project(s) Structure.

---
+++ tsmake.psd1
@@ -0,0 +1,37 @@
+﻿@{
+	RootModule = 'tsmake.psm1'
+	ModuleVersion = '0.3.5'
+	GUID = '9a6f967a-1f27-48d4-8838-802ff3bfad98'
+	Author = 'Michael K. Campbell'
+	CompanyName = 'OverAchiever Productions, LLC.'
+	Copyright = '(c) 2024. All rights reserved.'
+	Description = 'Module description'
+	
+	# Supported PSEditions
+	# CompatiblePSEditions = @('Core', 'Desktop')
+	
+	PowerShellVersion = '7.0'
+	DotNetFrameworkVersion = '8'
+	ProcessorArchitecture = 'None'
+	RequiredModules = @()
+	RequiredAssemblies = @()
+	ScriptsToProcess = @()
+	TypesToProcess = @()
+	FormatsToProcess = @()
+	NestedModules = @()
+	FunctionsToExport = '*' #For performance, list functions explicitly
+	CmdletsToExport = '*' 
+	VariablesToExport = '*'
+	AliasesToExport = '*' #For performance, list alias explicitly
+	ModuleList = @()
+	FileList = @()
+	PrivateData = @{
+		PSData = @{
+			# Tags = @()
+			# LicenseUri = ''
+			# ProjectUri = ''
+			# IconUri = ''
+			# ReleaseNotes = ''
+		}
+	}
+}
---
+++ tsmake.psm1
@@ -0,0 +1,52 @@
+﻿Set-StrictMode -Version 3.0;
+
+filter Import-Types {
+	param (
+		[string]$ScriptRoot = $PSScriptRoot
+	);
+	
+	# Import order CAN impact BUILD operations
+	$classFiles = @(
+		"$ScriptRoot\dotnet\tsmake\Globals.cs";
+#		"$ScriptRoot\dotnet\tsmake\Enums.cs";
+#		"$ScriptRoot\dotnet\tsmake\Errors.cs";
+#		"$ScriptRoot\dotnet\tsmake\Extensions.cs";
+#		
+#		"$ScriptRoot\dotnet\tsmake\models\Lines.cs";
+#		"$ScriptRoot\dotnet\tsmake\models\Tokens.cs";
+#		"$ScriptRoot\dotnet\tsmake\models\Directives.cs";
+#		
+#		"$ScriptRoot\dotnet\tsmake\models\Files.cs";
+#		"$ScriptRoot\dotnet\tsmake\Results.cs";
+#		
+#		"$ScriptRoot\dotnet\tsmake\Formatter.cs";
+	);
+	
+	Add-Type -Path $classFiles;
+}
+
+# Objects: 
+Import-Types;
+
+# Private Funcs: 
+foreach ($file in (@(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'internal/*.ps1') -Recurse -ErrorAction Stop))) {
+	try {
+		. $file.FullName;
+	}
+	catch {
+		throw "Unable to dot source INTERNAL tsmake file: [$($file.FullName)]`rEXCEPTION: $_  `r$($_.ScriptStackTrace) ";
+	}
+}
+
+# Public Funcs: 
+foreach ($file in (@(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'public/*.ps1') -Recurse -ErrorAction Stop))) {
+	try {
+		. $file.FullName;
+	}
+	catch {
+		throw "Unable to dot source PUBLIC tsmake file: [$($file.FullName)]`rEXCEPTION: $_  `r$($_.ScriptStackTrace) ";
+	}
+}
+
+Export-ModuleMember -Function Invoke-TsmBuild, Import-TsmTokens, Get-TsmToken, Remove-TsmTokens;
+Export-ModuleMember -Alias *;
