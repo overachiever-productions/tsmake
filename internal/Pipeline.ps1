54d034fc6f97ed523ada08850646323cc5ac6a2e
Michael K. Campbell <mike@overachiever.net>
2024-11-02 14:34:16 -0700

    Additional place-holders for CORE and supporter/helper files.

---
+++ internal/Pipeline.ps1
@@ -0,0 +1,19 @@
+﻿Set-StrictMode -Version 3.0;
+
+function Execute-Pipeline {
+	[CmdletBinding()]
+	param (
+		[PSCustomObject]$BuildContext
+	);
+	
+	begin {
+	};
+	
+	process {
+		
+	};
+	
+	end {
+		
+	};
+}
