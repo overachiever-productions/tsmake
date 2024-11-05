e1e6e5ba448970f77c08f0feda09bf9d8d55f6d3
Michael K. Campbell <mike@overachiever.net>
2024-11-05 09:32:29 -0800

    Bypassing need for extra 'intermediate' func
    i.e., added some sample calls/executions and ... simplified logic (direct to pipeline).

--- internal/Pipeline.ps1
+++ internal/Pipeline.ps1
@@ -3,13 +3,70 @@
 function Execute-Pipeline {
 	[CmdletBinding()]
 	param (
-		[PSCustomObject]$BuildContext
+		[ValidateSet("BUILD", "DOCS", "BOTH")]
+		[string]$Verb,
+		[string]$BuildFile,
+		[string]$Output,
+		$Version,
+		[PSCustomObject]$ConfigData,
+		[string[]]$Tokens,
+		[PSCustomObject]$Options,
+		[string]$WorkingDirectory
 	);
 	
 	begin {
+		[bool]$xVerbose = ("Continue" -eq $global:VerbosePreference) -or ($PSBoundParameters["Verbose"] -eq $true);
+		[bool]$xDebug = ("Continue" -eq $global:DebugPreference) -or ($PSBoundParameters["Debug"] -eq $true);
+		
+		# new-up a BuildResult object... 
+		# and... a file-handler. 
 	};
 	
 	process {
+		# ====================================================================================================
+		# 1. Create the Build Manifest (i.e., assemble ALL lines of code for processing):
+		# ====================================================================================================	
+		
+		[tsmake.SimpleFileSystem]$fileSystem = New-Object tsmake.SimpleFileSystem($WorkingDirectory);
+		[tsmake.Manifest]$manifest = New-Object tsmake.Manifest($fileSystem);
+		
+Write-Host "doing process stuff... ";
+		$manifest.LoadContents($BuildFile);
+		
+		$manifestLines = $manifest.ManifestLines;
+		
+		foreach ($line in $manifestLines) {
+			Write-Host "$($line.LineNumber) => $($line.LineText)";
+		}
+		
+		
+		
+		
+		
+		# Open the -BuildFile as a single, long, string and identify STRINGs and COMMENTS. 
+		# 	split by LINE. 
+		# 		for each line: 
+		# 			- set the lineage/source (i.e. line-number and file source)
+		# 			- if it's a directive to open up a new file... then: 
+		########			NOTE: 2 types of directives we're looking for here: FILE and DIRECTORY ... 
+		# 				- open file as single, long, string and identify STRINGs and COMMENTs + source/lineage. 
+		# 				- recurse... 
+		# 				- replace line in PARENT with ... this new content. 
+		# 
+		
+		# 	when the above is done we'll have:
+		# 		- an array of lines. 
+		# 		- where EACH LINE WILL HAVE: 
+		# 			- source / lineage 
+		# 			- contains and/or IS 'comment' or 'string' 
+		
+		# 	from the above ... 3 main tasks left: 
+		# 		1. for multi-line comments ... look for DOCUMENTATION and such. 
+		# 		2. for non-comments: 
+		# 			a. look for directives and process (i.e., primarily just versioning and such)
+		# 			b. look for and replace tokens as needed. 
+		
+		
 		
 	};
 	
--- public/Invoke.ps1
+++ public/Invoke.ps1
@@ -4,27 +4,36 @@
 
 	SIMPLEST EXECUTION OPTION (will find/detect a *.build.sql file in the current working directory (or will throw)): 
 		
-			Import-Module -Name "D:\Dropbox\Repositories\tsmake" -Force;		
-$global:VerbosePreference = "Continue";
-			# Set 'current' location = "..\test_files\simple" 
-			Set-Location (Get-Location | Split-Path -Parent | Join-Path -ChildPath "\test_files\simple1");
-			Invoke-TsmBuild; 
+				Import-Module -Name "D:\Dropbox\Repositories\tsmake" -Force;		
+			$global:VerbosePreference = "Continue";
+				# Set 'current' location = "..\test_files\simple" 
+				Set-Location (Get-Location | Split-Path -Parent | Join-Path -ChildPath "\test_files\simple1");
+				Invoke-TsmBuild; 
 
 
 	EXPLICIT EXECUTION OPTION (send in an explicitly defined *.build.sql file(name)): 
 
 
 
-	MULTI-BUILD (send in multiple files for build/processing): 
+	MULTI-BUILD VIA PIPELINE (send in multiple files for build/processing): 
 		
-			Import-Module -Name "D:\Dropbox\Repositories\tsmake" -Force;
-$global:VerbosePreference = "Continue";				
-			# point to 2x .build files (in the test_files folder): 
-			$testFilesBase = (Get-Location | Split-Path -Parent | Join-Path -ChildPath "\test_files");
-			$files = @("$($testFilesBase)\simple1\basic.build.sql", "$($testFilesBase)\simple2\my.build.sql");
-		
-			$files | Invoke-TsmBuild;
+				Import-Module -Name "D:\Dropbox\Repositories\tsmake" -Force;
+			$global:VerbosePreference = "Continue";				
+				# point to 2x .build files (in the test_files folder): 
+				$testFilesBase = (Get-Location | Split-Path -Parent | Join-Path -ChildPath "\test_files");
+				$files = @("$($testFilesBase)\simple1\basic.build.sql", "$($testFilesBase)\simple2\my.build.sql");
 			
+				$files | Invoke-TsmBuild;
+			
+	MULTI-BUILD VIA PARAMTERS (send in an array of paths as -BuildFile):
+
+				Import-Module -Name "D:\Dropbox\Repositories\tsmake" -Force;
+			$global:VerbosePreference = "Continue";				
+				# point to 2x .build files (in the test_files folder): 
+				$testFilesBase = (Get-Location | Split-Path -Parent | Join-Path -ChildPath "\test_files");
+				$files = @("$($testFilesBase)\simple1\basic.build.sql", "$($testFilesBase)\simple2\my.build.sql");
+			
+				Invoke-TsmBuild -BuildFile $files;
 
 	TOKENS (without explicit build-file) 
 
@@ -39,7 +48,15 @@ function Invoke-TsmBuild {
 		[Parameter(ValueFromPipeline)]
 		[string[]]$BuildFile,
 		[string]$ConfigFile,
-		[string]$Output,  				# TODO: Can't set -Output if/when multiple build-files are PIPED into this func - so... set up a parameter-set accordingly.
+		[string]$Output,  				
+
+		# Actually... what's the point of having a VERSION here as part of the build process? 
+		# do i NEED it? 
+		# obviously with things like admindb or ... dda i'm going to want it ... 
+		# 	BUT: 
+		# 		- how's that work if/when there are > 1 -BuildFiles specified? 
+		# 			and... if I can't build multiple projects at the same time when a version is specified ... why bother having a version and/or option to build multiple files? 
+		# 		- could I accomplish this in some other way - like with a -Token ??? 
 		[string]$Version, 				# TODO: Pass -Version in as a string and have C# code parse it to determing if Semantic, FourPart, or Organic/Custom...
 		[string[]]$Tokens,
 		[switch]$SkipDocumentation = $false
@@ -52,8 +69,6 @@ function Invoke-TsmBuild {
 	begin {
 		[bool]$xVerbose = ("Continue" -eq $global:VerbosePreference) -or ($PSBoundParameters["Verbose"] -eq $true);
 		[bool]$xDebug = ("Continue" -eq $global:DebugPreference) -or ($PSBoundParameters["Debug"] -eq $true);
-		
-		# TODO: process Version
 		
 		# TODO: build up an 'Options' object - which will track options for things like: 
 		# 		- skip/process file-marker, 
@@ -108,7 +123,6 @@ function Invoke-TsmBuild {
 		# ====================================================================================================
 		# Config-File(s):
 		# ====================================================================================================				
-		
 		# TODO: Implement logic for: 
 		# 		a. checking for .config file based on -BuildFile name/pattern. 
 		# 		b. checking for the same as above BUT when there are MULTIPLE files. 
@@ -120,11 +134,67 @@ function Invoke-TsmBuild {
 		# 		d. NO files. (nothing passed in and ...no matches via pattern.)
 		# 			and... ensure that the above works WHEN there are MULTIPLE -BuildFiles. 
 		
-		Write-Host "building stuff.. ";
+		# ====================================================================================================
+		# Output:
+		# ====================================================================================================		
+		# 	TODO: 
+		# 		Implement. 
+		# 				ALSO. 
+		# 				there CAN be a SINGLE -Output if/when multiple -BuildFile(s) are specified. 
+		# 				the idea would be something along the lines of admindb_latest.sql and admindb_azure_latest.sql ... going to the 
+		# 						same output/deployment folder somewhere... 
+		# 				i.e., see notes below for VERSION ... 
+		# 		OTHERWISE
+		# 			in terms of implementation
+		# 				all'z I need to do here is verify that the path in question (i.e., -Output) exists. 
+		# 				or, more specifically: 
+		# 					if -Output is a FOLDER ... and there are multiple -BuildFiles ... we're fine. 
+		# 							HOWEVER: the above ONLY works IF each .build.sql file in question has an OUTPUT directive OR a CONFIG-VALUE ... set for the file-name. 
+		# 					if -Output is a FILENAME ... and there are multiple -BuildFiles ... i'm pretty sure I have to THROW. 
+		# 							PRESUMABLY... if someone sends in a hard-coded path AND multiple -Build files... 
+		# 								i COULD 'overwrite' the file-name they've specified by means of the logic for the above (i.e., extract this info from .build.sql or CONFIG-FILE)
+		# 								BUT, I THINK that throwing is simply better - i.e., throw "woops - can't specify FILENAME when > 1 -BuildFile. Build 1 file at a time, or use ##output directive or a value in the .Config file instead (for multiples)."
 		
+		
+		# ====================================================================================================
+		# Version:
+		# ====================================================================================================			
+		# TODO: 
+		# 		IF there's a -Version specified then: 
+		# 			a) if we've got multiple -BuildFile entries... throw. 
+		# 				cuz you can't specify the same version for multiple build files ... or... can you? 
+		# 				i.e., what would be the use case here... i'm generating ... ah, 2 different build-files or outputs 
+		# 					like, say: admindb_latest.sql and admindb_azure_latest.sql 
+		# 					and... they need to be set to the SAME version. 
+		# 				so. yeah. ignore the notes above. 
+		# 				that said, if there are > 1 -BuildFile specified... 
+		# 					then do a Write-Verbose "N build files - all will use version XXX".
+		# 			b) parse/tease-apart the VERSION to see what kind it is is: Semantic, Four-Part, or Organic/Custom. 
+		
+		# ====================================================================================================
+		# Tokens:
+		# ====================================================================================================			
+		# TODO: 
+		# 		implement tokens
+		# 			(these are/were working SOLIDLY in v0.3)
+		# 		AND... the SAME tokens CAN be used against multiple -BuildFile inputs/build-files. 
+		# 			Likewise, the existence of a POTENTIAL .CONFIG file for each of the -BuildFiles that MIGHT be getting processed 
+		# 				would be one way to have entirely different tokens from one project to the next AND have some of the tokens that are the same. 
+		# 		STEPS: 
+		# 				1. reset/unregister any extant tokens (i.e., clear away anything from PREVIOUS runs)
+		# 				2. load base/stock/core tokens. 
+		# 				3. add in any tokens provided by the command-line and/or via the .config. 
+		
+		
+		foreach ($file in $buildFiles) {
+			#Write-Host "Building: [$file]"			
+			
+			
+			$results += Execute-Pipeline -Verb $verb -BuildFile $file -Output $Output -WorkingDirectory $pwd;
+		}
 	};
 	
 	end {
-		
+		return $results;
 	};
 }
