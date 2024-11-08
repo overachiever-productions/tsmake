a845dae78d72043ff13c3278cc6d478158323331
Michael K. Campbell <mike@overachiever.net>
2024-11-08 09:46:15 -0800

    New files for underlying pipeline (errors and results)

---
+++ dotnet/tsmake/Errors.cs
@@ -0,0 +1,108 @@
+﻿using System.Management.Automation;
+
+namespace tsmake;
+
+public interface IError
+{
+    ErrorType ErrorType { get; }
+    ErrorRecord ErrorRecord { get; }
+    
+    ISourceLine SourceLine { get; } // i.e., the location 
+
+    string Phase { get; }
+    string Facet { get; }
+    string Detail { get; }
+
+    string Message { get; }
+
+    string Summarize();      // used by xml formatters/etc. 
+}
+
+public class Error : IError
+{
+    public ErrorType ErrorType { get; }
+    public ErrorRecord ErrorRecord { get; }
+    public ISourceLine SourceLine { get; }
+    public string Phase { get; }
+    public string Facet { get; }
+    public string Detail { get; }
+    public string Message { get; }
+
+    private Error(ErrorRecord errorRecord, ErrorType type, SourceLine sourceLine, string phase, string message, string facet = "", string detail = "")
+    {
+        this.ErrorType = type;
+        this.ErrorRecord = errorRecord;
+        this.SourceLine = sourceLine;
+        this.Phase = phase;
+        this.Facet = facet;
+        this.Detail = detail;
+        this.Message = message;
+    }
+
+    private Error(ErrorType type, SourceLine sourceLine, string phase, string message, string facet = "", string detail = "")
+    {
+        this.ErrorType = type;
+        this.SourceLine = sourceLine;
+        this.Phase = phase;
+        this.Facet = facet;
+        this.Detail = detail;
+        this.Message = message;
+    }
+
+    public static Error NewRuntimeError(ErrorRecord errorRecord, SourceLine sourceLine, string phase, string message, string facet = "", string detail = "")
+    {
+        return new Error(errorRecord, ErrorType.Runtime, sourceLine, phase, message, facet, detail);
+    }
+
+    public static Error NewConfigurationError(SourceLine sourceLine, string phase, string message, string facet = "", string detail = "")
+    {
+        return new Error(ErrorType.Configuration, sourceLine, phase, message, facet, detail);
+    }
+
+    //public static Error NewValidationError()
+    //{
+
+    //}
+
+    public static Error NewSyntaxError(SourceLine sourceLine, string phase, string message, string facet = "", string detail = "")
+    {
+        return new Error(ErrorType.Syntax, sourceLine, phase, message, facet, detail);
+    }
+
+    //public static Error NewBuildError()
+    //{
+    //}
+
+    //public static Error NewDocumentationError()
+    //{
+    //}
+
+    public string Summarize()
+    {
+        throw new NotImplementedException();
+
+        // something along the lines of Option C makes the most sense... 
+
+
+        // OPTION A (phase is a different line)
+        // {ErrorType} Error:
+        //    <phase> (<facet if there is one>)
+        //      <file-name.ext>, line ##  (offsets if i have them)
+        //      <message> 
+        //      <detail/etc.>
+
+        // OPTION B (phase is part of the error 'type')
+        // {ErrorType} Error | {Phase (facet if there is one)} 
+        //      {location stuff - as above}
+        //      {message}
+        //      {detail}
+
+        // OPTION C - phases as a GROUPING mechanism out/above error types: 
+        // {Phase}
+        //      {ErrorType} Error: 
+        //          {Facet} 
+        //          {location}
+        //          {message}
+        //          {detail}
+    }
+}
