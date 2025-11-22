87351e9a3860b6ac4e5180bedd4c997f9303fd35
Michael K. Campbell <mike@overachiever.net>
2025-11-22 08:28:55 -0800

    Re-Rewrite.
    Repo-cleanup.

--- dotnet/tsmake.tests/Usings.cs
+++
@@ -1,4 +0,0 @@
-﻿global using System.Text.RegularExpressions;
-global using NUnit.Framework;
-//global using tsmake.models;
-global using Moq;
--- test_files/calibration1/six-sixes.sql
+++
@@ -1,8 +0,0 @@
--- 6.6.0
--- 6.6.1
--- 6.6.2
--- 6.6.3
--- 6.6.4
-  'busted string
-that wraps
-and stuff
