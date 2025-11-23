7210c2e2e75e44c6fbef542434d01ac77c412beb
Michael K. Campbell <mike@overachiever.net>
2025-11-22 17:14:03 -0800

    TMS-24: Stubs for Unit Tests
    Addition of NUnit Project + stubs.

---
+++ dotnet/tsmake.tests/Usings.cs
@@ -0,0 +1,3 @@
+﻿global using System.Text.RegularExpressions;
+global using NUnit.Framework;
+global using Moq;
---
+++ dotnet/tsmake.tests/unit_tests/normalizer/normalizer.cs
@@ -0,0 +1,34 @@
+﻿namespace tsmake.tests.unit_tests.normalizer;
+
+public class NormalizerTests
+{
+    [Test]
+    public void It_Splits_On_CrLf()
+    {
+        Assert.Fail("Not implemented");
+    }
+
+    [Test]
+    public void It_Splits_On_Lf()
+    {
+        Assert.Fail("Not implemented");
+    }
+
+    [Test]
+    public void It_Splits_On_Cr()
+    {
+        Assert.Fail("Not implemented");
+    }
+
+    [Test]
+    public void It_Splits_On_Mixed_Line_Endings()
+    {
+        Assert.Fail("Not implemented");
+    }
+
+    [Test]
+    public void It_Collapses_To_Specified_CrLfOptions()
+    {
+        Assert.Fail("Not implemented");
+    }
+}
