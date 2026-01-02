37afc1d46e02b5071767f963885e83b70dd5d126
Michael K. Campbell <mike@overachiever.net>
2026-01-02 09:49:18 -0800

    TSM-25: Addition of files for Mapper Functionality.
    Attempting a manual 'recovery' of a failed stash fiasco...

--- dotnet/tsmake/Globals.cs
+++ dotnet/tsmake/Globals.cs
@@ -5,6 +5,7 @@ global using System.Linq;
 global using System.Text;
 global using System.Management.Automation;
 global using System.Text.RegularExpressions;
+global using System.ComponentModel;
 
 namespace tsmake;
 
